0b9ef7dbc5fda95ce4614013978303a1c706eb68
Alex <examle@gmail.com>
2021-02-20 16:48:04 +0300

    mod js file about actors

--- js/script.js
+++ js/script.js
@@ -9,9 +9,12 @@ const personalMovieDB = {
 };
 const a = prompt('Один из последних просмотренных фильмов?', ''),
       b = prompt('На сколько вы его оцените?', ''),
+      e = prompt('Помните актера?', ''),
       c = prompt('Один из последних просмотренных фильмов?', ''),
-      d = prompt('На сколько вы его оцените?', '');
+      d = prompt('На сколько вы его оцените?', ''),
+      f = prompt('Оценка актрера?', '');
 
       personalMovieDB.movies[a] = b;
       personalMovieDB.movies[c] = d;
+      personalMovieDB.actors[e] = f;
       console.log(personalMovieDB);
